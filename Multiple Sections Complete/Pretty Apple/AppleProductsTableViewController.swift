fb43f85fe05c425d0d64486914d81ce4c6df8c8c
Developer Inspirus <duc@developerinspirus.com>
2015-04-03 18:14:20 -0400

    UITableView Series Tutorial 3
    UITableView Series Tutorial 3

---
+++ Multiple Sections Complete/Pretty Apple/AppleProductsTableViewController.swift
@@ -0,0 +1,93 @@
+//
+//  AppleProductsTableViewController.swift
+//  Pretty Apple
+//
+//  Created by Duc Tran on 3/28/15.
+//  Copyright (c) 2015 Duc Tran. All rights reserved.
+//
+
+import UIKit
+
+class AppleProductsTableViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // MARK: - Data Source
+    
+    lazy var productLines: [ProductLine] = {
+        return ProductLine.productLines()
+    }()
+ 
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let productLine = productLines[section]
+        return productLine.name
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return productLines.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let productLine = productLines[section]
+        return productLine.products.count   // the number of products in the section
+    }
+
+    // indexPath: which section and which row
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Product Cell", forIndexPath: indexPath) as UITableViewCell
+
+        let productLine = productLines[indexPath.section]
+        let product = productLine.products[indexPath.row]
+        cell.textLabel?.text = product.title
+        cell.detailTextLabel?.text = product.description
+        cell.imageView?.image = product.image
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
